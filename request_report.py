e2882d451320ae6897dda42024c40027ae51c85f
abhinavkeshari <58878885+abhinavkeshari@users.noreply.github.com>
2022-04-28 15:25:36 +0530

    save

--- request_report.py
+++ request_report.py
@@ -9,7 +9,7 @@ api_key = settings.crimecheck_api_key
 
 
 
-def request_report(cin: str, companyName: str, compAddress: str):
+def request_report(cin: str, companyName: str, compAddress: str, directors: list = None) -> str:
     headers = {
         'content-type': 'application/x-www-form-urlencoded',
     }
@@ -37,17 +37,20 @@ def request_report(cin: str, companyName: str, compAddress: str):
         ('callbackUrl',settings.callback_url),
         ('companyAddress', compAddress)
     ]
-
-
+    if directors:
+        data.append(('directors', json.dumps(directors)))
     # url_encoded_query = "searchTerm=" + urllib.parse.quote_plus(json.dumps(data2))+"&page=1&resultsPerPage=100&matchFlag=true&matchingCriteria=true&callbackUrl=http://api.altinfo.com/api/callback/crimecheck"
     response = requests.post('https://crime.getupforchange.com/api/v3/addReport', headers=headers, data=data,auth=(api_key, ''))
     print(response.status_code)
     return response.text
 
 
-def report(CIN:str, companyName: str, companyAddress: str):
+def report(cin:str, companyName: str, companyAddress: str, directors: list = None):
     with open(f'{settings.base_data_path}/response.json', 'w',encoding='utf-8') as response_file:
-        response_json = json.loads(request_report(cin=CIN, companyName=companyName, compAddress=companyAddress))
+        response_json = json.loads(request_report(cin=cin,
+                                                  companyName=companyName,
+                                                  compAddress=companyAddress,
+                                                  directors=directors))
         loaded_memory = {}
         memory_file_name = f'{settings.base_data_path}/memory.yml'
         if(not os.path.isfile(memory_file_name)):
@@ -62,7 +65,7 @@ def report(CIN:str, companyName: str, companyAddress: str):
 
         with open(f'{settings.base_data_path}/memory.yml', 'w') as memory:
             if "requestId" in response_json.keys():
-                loaded_memory['reports'].append(response_json['requestId']  + " " + CIN)
+                loaded_memory['reports'].append(response_json['requestId']  + " " + cin)
                 yaml.dump(loaded_memory, memory)
             else:
                 print("request failed " + str(response_json) )
@@ -72,33 +75,66 @@ def report(CIN:str, companyName: str, companyAddress: str):
 if __name__ == '__main__':
     orders = [
         # {
-        #     "cin": "U70100DL2001PTC166343",
-        #     "companyName": "LOGIX SOFT-TEL PRIVATE LIMITED",
-        #     "companyAddress": "DGL006, Ground Floor, DLF Galleria, Mayur Vihar, Phase-I New Delhi East Delhi 110091"
+        #     "cin": "U74899DL1983PTC017237",
+        #     "companyName": "VIMAL PLAST (INDIA) PRIVATE LIMITED",
+        #     "companyAddress": "PROPERTY NO. E-21, FIRST FLOOR, NARAINA VIHAR, NEW DELHI South West Delhi 110028",
+        #     "directors": [
+        #         {
+        #             "name" : "Harish Batra",
+        #             "fatherName": "Khushi Ram Batra",
+        #             "dob": "",
+        #             "address": "F-50 D, RADHE MOHAN DRIVE BANDH ROAD, VILLAGE- JAUNPUR, JONAPUR 110047",
+        #             "panNumber": "AAGPB7869J"
+        #         },
+        #         {
+        #             "name": "Saurabh Batra",
+        #             "fatherName": "Harish Batra",
+        #             "dob": "",
+        #             "address": "F-50 D, RADHE MOHAN DRIVE BANDH ROAD, VILLAGE- JAUNPUR, JONAPUR 110047",
+        #             "panNumber": "AFAPB4359Q"
+        #         },
+        #         {
+        #             "name": "Anju Batra",
+        #             "fatherName": "Sada Nand Grover",
+        #             "dob": "",
+        #             "address": "F-50 D, RADHE MOHAN DRIVE BANDH ROAD, VILLAGE- JAUNPUR, JONAPUR 110047",
+        #             "panNumber": "AHNPB0173B"
+        #         },
+        #         {
+        #             "name": "Sahil Batra",
+        #             "fatherName": "Harish Batra",
+        #             "dob": "",
+        #             "address": "F-50 D, RADHE MOHAN DRIVE BANDH ROAD, VILLAGE- JAUNPUR, JONAPUR 110047",
+        #             "panNumber": "AHNPB0146J"
+        #         }
+        #     ]
         # },
-        # {
-        #     "cin": "U45400DL2002PTC114401",
-        #     "companyName": "IT ENFRASERVICES PRIVATE LIMITED",
-        #     "companyAddress": "DGL006, Ground Floor, DLF Galleria, Mayur Vihar, Phase-I New Delhi East Delhi 110091"
-        # },
-        # {
-        #     "cin": "U51909TG2021PTC153617",
-        #     "companyName": "HORNBILLE MACHINES AND RENTALS PRIVATE LIMITED",
-        #     "companyAddress": "PLOT NO 28 29 30, GNR RV INSIGNIA SILICON VALLEY LAYOUT,MADHAPUR Hyderabad Telangana 500034"
-        # },
+        {
+            "cin": "L26940MH1936PLC002515",
+            "companyName": "ACC LIMITED",
+            "companyAddress": "CEMENT HOUSE 121MAHARSHI KARVE ROAD MUMBAI Maharashtra"
+        },
+        {
+            "cin": "L26942GJ1981PLC004717",
+            "companyName": "AMBUJA CEMENTS LIMITED",
+            "companyAddress": "P O AMBUJA NAGAR, TALUKA KODINAR, AMRELI, DIST JUNAGADH Gujarat 362715"
+        },
         # {
         #     "cin": "U74999MH2015PTC266456",
         #     "companyName": "FLIPSPACES TECHNOLOGY LABS PRIVATE LIMITED",
         #     "companyAddress": "Unit Nos. 801B, 801C and 802A, 8th Floor, Eureka Tower,Mindspace , Link Road, Malad (West), Mumbai City Maharashtra 400064"
         # },
         # {
-        #     "cin": "U45400MH2011PTC222160",
+        #     "cin": "U45200MH1982PTC028164",
         #     "companyName": "GLOOB INTERIOR DESIGN PRIVATE LIMITED",
         #     "companyAddress": "Unit Nos. 801B, 801C and 802A, 8th Floor, Eureka Tower,Mindspace , Link Road, Malad (West), Mumbai City Maharashtra 400064"
         # }
     ]
     for order in orders :
-        report(order['cin'], order['companyName'], order['companyAddress'])
+        report(cin=order['cin'],
+               companyName=order['companyName'],
+               companyAddress=order['companyAddress'],
+               directors=None)
         time.sleep(2)
 
 
---
+++ request_report_individual.py
@@ -0,0 +1,77 @@
+import json
+from config.environment import settings
+import requests
+import yaml
+import os
+import time
+import urllib.parse
+api_key = settings.crimecheck_api_key
+
+
+
+def request_report(query: dict) -> str:
+    headers = {
+        'content-type': 'application/x-www-form-urlencoded',
+    }
+
+
+
+
+    data = [
+        ('searchTearm', json.dumps(query)),
+    ]
+    response = requests.post('https://crime.getupforchange.com/api/v3/records', headers=headers, data=data,auth=(api_key, ''))
+    print(response.status_code)
+    return response.text
+
+
+def report_individual(query: dict):
+    with open(f'{settings.base_data_path}/response.json', 'w',encoding='utf-8') as response_file:
+        response_json = json.loads(request_report(query=query))
+        loaded_memory = {}
+        memory_file_name = f'{settings.base_data_path}/memory_individual.yml'
+        if(not os.path.isfile(memory_file_name)):
+            memory_file = open(memory_file_name, 'w')
+            memory_file.close()
+        with open(f'{settings.base_data_path}/memory_individual.yml', 'r') as memory:
+            memory_from_file = yaml.safe_load(memory)
+            if memory_from_file:
+                loaded_memory.update(memory_from_file)
+            if( not 'reports' in loaded_memory.keys()):
+                loaded_memory['reports'] = []
+
+        with open(f'{settings.base_data_path}/memory_individual.yml', 'w') as memory:
+            if "requestId" in response_json.keys():
+                loaded_memory['reports'].append(response_json['requestId']  + " " + query['name'])
+                yaml.dump(loaded_memory, memory)
+            else:
+                print("request failed " + str(response_json) )
+
+        json.dump(response_json, response_file, indent=4)
+
+if __name__ == '__main__':
+    orders = [
+        {
+            "name" : "Mayank Chawla",
+            "fatherName" : "Rakesh Chawla",
+            "address" : "385, KOHAT ENCLAVE, PITAM PURA, DELHI 110034"
+        },
+        {
+            "name" : "Rakesh Chawla",
+            "fatherName": "Chaman Lal Chawla",
+            "address" : "385, KOHAT ENCLAVE, PITAM PURA, DELHI 110034"
+        },
+        {
+            "name": "Anshu Nagpal",
+            "fatherName" : "Kundal Lal",
+            "address" : "Mal Godam Road Moh - Shivpuri, Gajraula Dhanaura Amroha Uttar Pradesh 244235"
+        }
+    ]
+    for order in orders :
+        report_individual(query=order)
+        time.sleep(2)
+
+
+
+
+
